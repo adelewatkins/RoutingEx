96637c9bafacbb7d65c271a6cc76d7019ed73b85
adelewatkins <adele_watkins@hotmail.com>
2023-12-01 12:15:03 +0000

    inital routing task

---
+++ src/Components/About.jsx
@@ -0,0 +1,18 @@
+
+function About() {
+    return (
+        <div>
+
+            <h1>About Page</h1>
+            <h3>
+               <p> Name: Adele</p>
+               <p> Age: 27</p>
+               <p> Job: LBG</p>
+               <p> Current State: Confused</p>
+            </h3>
+        </div>
+
+    );
+}
+
+export default About;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,11 @@
+
+function Home() {
+    return (
+        <div>
+            <h1>HOME PAGE</h1>
+            <h2>Welcome</h2>
+        </div>
+    );
+}
+
+export default Home;
